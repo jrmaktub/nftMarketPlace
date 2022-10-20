9a2732c3f2a7ec9f4608b8a04090a17fa3a5bcc8
jramirez09 <jramirez12@luc.edu>
2022-10-20 15:12:13 -0500

    contratos y pruebas terminados sin problemas

--- src/backend/scripts/deploy.js
+++ src/backend/scripts/deploy.js
@@ -5,12 +5,40 @@ async function main() {
   console.log("Deploying contracts with the account:", deployer.address);
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  // deploy contracts here:
+  // // deploy contracts here:
+  // const NFT = await ethers.getContractFactory("NFT");
+  // const nft = await NFT.deploy();
+  // const Marketplace = await ethers.getContractFactory("Marketplace");
+  // const marketplace = await Marketplace.deploy(1);
+
+
+
   
-  
-  // For each contract, pass the deployed contract and name to this function to save a copy of the contract ABI and address to the front end.
-  saveFrontendFiles();
+  // // For each contract, pass the deployed contract and name to this function to save a copy of the contract ABI and address to the front end.
+  // saveFrontendFiles(nft, "NFT");
+  // saveFrontendFiles(marketplace, "Marketplace");
+
+  // Get the ContractFactories and Signers here.
+  const NFT = await ethers.getContractFactory("NFT");
+  const Marketplace = await ethers.getContractFactory("Marketplace");
+  // deploy contracts
+  const marketplace = await Marketplace.deploy(1);
+  const nft = await NFT.deploy();
+  // Save copies of each contracts abi and address to the frontend.
+  saveFrontendFiles(marketplace , "Marketplace");
+  saveFrontendFiles(nft , "NFT");
+
+  console.log("NFT contract address: ", nft.address);
+  console.log("Marketplace contract address: ", marketplace.address);
 }
+// npx hardhat run src/backend/scripts/deploy.js --network localhost
+//const contract = await ethers.getContractAt("NFT","0x5FbDB2315678afecb367f032d93F642f64180aa3")
+//const contract = await ethers.getContractAt("NFT","0xA51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0")
+//const tokenCount = await contract.tokenCount()
+//npx hardhat console --network localhost
+//const marketplace = await ethers.getContractAt("Marketplace","0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0")
+//const marketplace = await ethers.getContractAt("Marketplace","0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e")
+
 
 function saveFrontendFiles(contract, name) {
   const fs = require("fs");
