ca55633674437e6f42a5829915518f040f70aecf
jramirez09 <jramirez12@luc.edu>
2022-10-20 16:58:53 -0500

    limpeando

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -1,20 +1,73 @@
-
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 import logo from './logo.png';
 import './App.css';
+import Navigation from './Navbar';
+import Home from './Home.js'
+import Create from './Create.js'
+import MyListedItems from './MyListedItems.js'
+import MyPurchases from './MyPurchases.js'
+import { useState } from 'react'
+import {ethers} from 'ethers'
+import MarketplaceAbi from '../contractsData/Marketplace.json'
+import MarketplaceAddress from '../contractsData/Marketplace-address.json'
+import NFTAbi from '../contractsData/NFT.json'
+import NFTAddress from '../contractsData/NFT-address.json'
+import { Spinner } from 'react-bootstrap'
  
 function App() {
+  const [loading, setLoading] = useState(true)
+  const [account, setAccount] = useState(null)
+  const [nft, setNFT] = useState({})
+  const [marketplace, setMarketplace] = useState({})
+//metamask connect
+  const web3Handler = async ()=>{
+    //get provider from Metamask
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    setAccount(accounts[0])
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+    loadContracts(signer)
+  }
+
+  const loadContracts = async (signer) => {
+    // Get deployed copies of contracts
+    const marketplace = new ethers.Contract(MarketplaceAddress.address, MarketplaceAbi.abi, signer)
+    setMarketplace(marketplace)
+    const nft = new ethers.Contract(NFTAddress.address, NFTAbi.abi, signer)
+    setNFT(nft)
+    setLoading(false)
+  }
+
   return (
+    <BrowserRouter>
     <div>
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a
-          className="navbar-brand col-sm-3 col-md-2 ms-3"
-          href="http://www.dappuniversity.com/bootcamp"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Dapp University
-        </a>
-      </nav>
+        <Navigation web3Handler={web3Handler} account={account}/>
+        {loading ? (
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '80vh' }}>
+              <Spinner animation="border" style={{ display: 'flex' }} />
+              <p className='mx-3 my-0'>Awaiting Metamask Connection...</p>
+            </div>
+          ) : (
+            <Routes>
+            <Route path="/" element={
+              <Home marketplace={marketplace} nft={nft} />
+            } />
+            <Route path="/create" element={
+              <Create marketplace={marketplace} nft={nft} />
+            } />
+            <Route path="/my-listed-items" element={
+              <MyListedItems marketplace={marketplace} nft={nft} account={account} />
+            } />
+            <Route path="/my-purchases" element={
+              <MyPurchases marketplace={marketplace} nft={nft} account={account} />
+            } />
+          </Routes>
+          )}
+
       <div className="container-fluid mt-5">
         <div className="row">
           <main role="main" className="col-lg-12 d-flex text-center">
@@ -43,6 +96,7 @@ function App() {
         </div>
       </div>
     </div>
+    </BrowserRouter>
   );
 }
 
